8bfef09ed3e8b4dd30e29bec48648545f6ba268d
Arthur <arthur.nascimento2020@outlook.com>
2022-07-03 15:14:07 -0300

    teste de link

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,14 @@
-import { useState } from 'react'
+import Link from "next/link";
 
 function Home() {
     return (
-        <div>
-            <h1>Home</h1>
-            <Contador />
-            <div>Teste</div>
-        </div>
-    )
-}
-
-function Contador() {
-    const [contador, setContador] = useState(1);
-
-    function adicionarContador() {
-        setContador(contador + 1);
-    }
-
-    return (
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionarContador}>Adicionar</button>
-        </div>
+    <div>
+        <h1>Home</h1>
+        <Link href='/sobre'>
+            <a>Acessar página Sobre</a>
+        </Link>
+        
+    </div>
     )
 }
 
